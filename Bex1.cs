7c69f0f8117792c5ea0ce9074e1dbd6b123a52e9
Moonyll <s.pouwel@laposte.net>
2018-12-18 11:54:28 +0100

    Bex1

---
+++ Bex1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int nombre;
+        for (nombre = 0; nombre < 10; nombre++)
+            {
+                Console.WriteLine("Bonjour, je ne suis qu'un simple message.");
+            }
+        }
+    }
+}
