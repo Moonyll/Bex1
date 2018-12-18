dd108350ef0d7847d12bd4d9d1d4e415830d226f
Moonyll <s.pouwel@laposte.net>
2018-12-18 12:44:40 +0100

    Updated

---
+++ Bex1'.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while (i < 10)
+            {
+            Console.WriteLine("Bonjour, je ne suis qu'un simple message.");
+            i++;
+            }
+        
+        }
+    }
+}
